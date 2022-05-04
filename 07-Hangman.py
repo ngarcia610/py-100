6836e999bc50bc0afeb32e808ace8daf06904fdb
Nathan Garcia <nategarcia.va@gmail.com>
2022-05-04 15:03:55 -0400

    Day 7 Hangman, Added random word

---
+++ 07-Hangman.py
@@ -0,0 +1,13 @@
+# Day 7 - Hangman Game
+from random_word import RandomWords
+r = RandomWords()
+
+# Randomly choose a word and assign it to $chosen_word
+chosen_word = str(r.get_random_word())
+print(chosen_word)
+
+# Have the user guess a letter and assign it to $guess
+
+
+# Check if the letter guessed is in the $chosen_word
+

e5aad4617f4d68d8f72494b7dd62217725602a0a
Nathan Garcia <nategarcia.va@gmail.com>
2022-06-04 15:19:56 -0400

    Added directories, fixed 07 Hangman, removed examples

--- examples/leap-year-check.py
+++
@@ -1,8 +0,0 @@
-# Takes the year as input and checks if it is a leap year
-# Returns True or False
-
-def is_leap(year):
-  return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
-
-year = int(input("Enter a year: "))
-print(is_leap(year))
--- examples/loops.py
+++
@@ -1,8 +0,0 @@
-def count_down(start_number):
-  current = start_number
-  while (current > 0):
-    print(current)
-    current -= 1
-  print("Zero!")
-
-count_down(3)

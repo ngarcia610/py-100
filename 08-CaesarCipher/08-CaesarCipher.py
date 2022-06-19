d7d37dbf8cc8b2452cc502d628d42fe609609e6e
Nathan Garcia <nategarcia.va@gmail.com>
2022-06-19 14:06:12 -0400

    Day 8 Caesar Cipher

---
+++ 08-CaesarCipher/08-CaesarCipher.py
@@ -0,0 +1,37 @@
+# Example Caesar Cipher Program
+from art import logo
+
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  # Check for non letter characters
+  # if char is a non letter, append it to the final text
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"The {cipher_direction}d text is {end_text}")
+
+should_continue = True
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  if shift == 26:
+    shift += 1
+  elif shift > 26:
+    shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  result = input("Type 'y' if you want to go again. Otherwise type 'n'.\n").lower()
+  if result == "n":
+    should_continue = False
+    print("Goodbye")

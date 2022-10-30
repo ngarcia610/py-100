ddfd3b5ebfa184a76a882f730ebdf0bc3e1e1ca5
Nathan Garcia <nategarcia.va@gmail.com>
2022-10-30 05:48:26 -0400

    Switched back to VS Code

---
+++ 16-OOP/OOP.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red", "green")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()

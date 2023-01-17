e395f4dee8975a51fd82645348bf84d9355629e5
Nathan Garcia <nategarcia.va@gmail.com>
2023-01-17 13:04:10 -0500

    "Day 19 - Turtle Race"

---
+++ 19-Turtle2/example.py
@@ -0,0 +1,41 @@
+"""
+Example program to show high order functions and event listeners.
+High order function means a function has another function as a parameter,
+ or returns a function as its output.
+Here the onkey function uses the movement functions with a key press.
+Note you don't include the parenthesis for the movement function.
+"""
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
+screen.exitonclick()

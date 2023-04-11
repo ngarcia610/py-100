f0a1325b1c42f09bd88301b428dddb60f75ee2b5
Nathan Garcia <nategarcia.va@gmail.com>
2023-04-11 12:39:28 -0400

    "Day 22 finished"

--- 22-Pong/main.py
+++ 22-Pong/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 # Create the screen
 screen = Screen()
@@ -10,6 +13,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -17,9 +22,32 @@ screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
+
+
 # Draw the paddle and refresh the screen after screen.tracer(0)
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    # Detect collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # Bounce the ball when it collides with the wall
+        ball.bounce_y()
+
+    # Detect collision with paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle miss
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L paddle miss
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()

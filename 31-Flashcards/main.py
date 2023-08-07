1df2e1510b0df2db977b28cc37f8cf48e9033f5d
Nathan Garcia <nategarcia.va@gmail.com>
2023-08-07 10:55:45 -0400

    Day 31 Adds UI and card behavior

--- 31-Flashcards/main.py
+++ 31-Flashcards/main.py
@@ -1,2 +1,63 @@
-BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+import pandas
+import random
 
+BACKGROUND_COLOR = "#B1DDC6"
+CARD_TIME = 3000
+
+# Use Pandas to get words and translations from the csv
+# Use the 'orient records' parameter to pair the spanish and english words
+# This returns a list of dictionaries
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_dict.html
+data = pandas.read_csv("data/spanish_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text = "Spanish", fill="black")
+    canvas.itemconfig(card_word, text = current_card["Spanish"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(CARD_TIME, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# Flip to the back of the card
+# This calls it the first time.
+flip_timer = window.after(CARD_TIME, func=flip_card)
+
+# Create a canvas layer to add the card
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+# Create the buttons
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button.grid(row=1, column=1)
+
+# Fill in the title and word for the first time after the UI is created
+next_card()
+
+window.mainloop()

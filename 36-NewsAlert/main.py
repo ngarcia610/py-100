d3947c86b0a7a31781e11832f3070cc2e41e349e
Nate Garcia <nategarcia.va@gmail.com>
2024-09-08 16:36:07 -0400

    "Day 36 Start"

---
+++ 36-NewsAlert/main.py
@@ -0,0 +1,6 @@
+# Objectives:
+# Pull in the stock prices of the stocks we're interested in
+# Find the difference between two days and the direction
+# Calculate the percentage difference
+# Fetch the relevant news data
+# Send an SMS with the percentage and news

c0a9736f4733f5167590e2b170dc7f0c75296c14
Nate Garcia <nategarcia.va@gmail.com>
2024-12-27 17:22:28 -0500

    Day 37 Complete

--- 37-HabitTracker/main.py
+++ 37-HabitTracker/main.py
@@ -1,6 +1,7 @@
 # Pixela: Record and track your habits by API
 # https://pixe.la/
 # https://docs.pixe.la/
+# Optional: Update the create pixel request to ask for user input
 
 import requests
 from datetime import datetime
@@ -8,9 +9,7 @@ from datetime import datetime
 USERNAME = "nate100"
 TOKEN = ""
 GRAPH_ID = "graph1"
-pixela_endpoint = "https://pixe.la/v1/users"
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
 
 #1. Create User
 user_params = {
@@ -20,8 +19,10 @@ user_params = {
   "notMinor": "yes",
 }
 
+pixela_endpoint = "https://pixe.la/v1/users"
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
 
 #2. Create Graph Config
 graph_config = {
@@ -36,11 +37,12 @@ headers = {
   "X-USER-TOKEN": TOKEN
 }
 
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
-#3. Create pixel
 
+#3. Create pixel
 today = datetime(year=2024, month=12, day=27)
 
 pixel_config = {
@@ -48,5 +50,22 @@ pixel_config = {
   "quantity": "5",
 }
 
-response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
-print(response.text)
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+# response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+# print(response.text)
+
+
+#4. Update a graph
+update_pixel = {
+  "quantity": "4"
+}
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+# response = requests.put(url=update_endpoint, json=update_pixel, headers=headers)
+# print(response.text)
+
+
+#5. Delete a pixel
+del_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+# response = requests.delete(url=del_endpoint, headers=headers)
+# print(response.text)

722a205fb5518fab7916ca6a746d5ff3c7eeac33
Nate Garcia <nategarcia.va@gmail.com>
2025-01-09 20:55:32 -0500

    Completes 38.2

--- 38-ExerciseTracking/main.py
+++ 38-ExerciseTracking/main.py
@@ -1,6 +1,29 @@
 import creds
+import requests
 
 API_KEY = creds.API_KEY
 APP_ID = creds.APP_ID
+URL = "https://trackapi.nutritionix.com/v2/natural/exercise"
+GENDER = "male"
+WEIGHT = 90.7
+HEIGHT = 173.7
+AGE = 39
 
-print(APP_ID, API_KEY)
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+  "x-app-id": APP_ID,
+  "x-app-key": API_KEY
+}
+
+params = {
+  "query": exercise_text,
+  "gender": GENDER,
+  "weight_kg": WEIGHT,
+  "height_cm": HEIGHT,
+  "age": AGE
+}
+
+response = requests.post(URL, json=params, headers=headers)
+result = response.json()
+print(result)

acbb13733a3fc447a972fa9513b050d4a9e01f5e
Nate Garcia <nategarcia.va@gmail.com>
2025-03-08 16:25:50 -0500

    Adds info for Day 40

---
+++ 40-FlightClub/main.py
@@ -0,0 +1,26 @@
+# more code above
+
+
+    # ==================== Send Notifications and Emails  ====================
+
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        # Customise the message depending on the number of stops
+        if cheapest_flight.stops == 0:
+            message = f"Low price alert! Only GBP {cheapest_flight.price} to fly direct "\
+                      f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
+                      f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        else:
+            message = f"Low price alert! Only GBP {cheapest_flight.price} to fly "\
+                      f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
+                      f"with {cheapest_flight.stops} stop(s) "\
+                      f"departing on {cheapest_flight.out_date} and returning on {cheapest_flight.return_date}."
+
+        print(f"Check your email. Lower price flight found to {destination['city']}!")
+
+        # notification_manager.send_sms(message_body=message)
+        # SMS not working? Try whatsapp instead.
+        notification_manager.send_whatsapp(message_body=message)
+
+        # Send emails to everyone on the list
+        notification_manager.send_emails(email_list=customer_email_list, email_body=message)
+

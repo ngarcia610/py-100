66f32792b2ac348439e33252613a06d0f46d919c
Nathan Garcia <nategarcia.va@gmail.com>
2025-08-08 18:01:24 -0400

    Starts Day 46

---
+++ 46-MusicTimeMachine/main.py
@@ -0,0 +1,4 @@
+# Use beautiful soup to scrape the billboard top 100 for a given date
+# Extract all song titles from the list
+# Use the Spotify API to create a new playlist for that particular date
+# Search Spotify for each of those songs and add them to a playlist

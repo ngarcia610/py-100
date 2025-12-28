01ab1575e9861e357152327de0e31e9556dc7458
Nathan Garcia <nategarcia.va@gmail.com>
2025-12-27 20:03:52 -0500

    Completes Day 46

--- 46-MusicTimeMachine/main.py
+++ 46-MusicTimeMachine/main.py
@@ -2,36 +2,92 @@
 # Extract all song titles from the list
 # Use the Spotify API to create a new playlist for that particular date
 # Search Spotify for each of those songs and add them to a playlist
-
+from bs4 import BeautifulSoup
 import requests
 import spotipy
-from bs4 import BeautifulSoup
 from spotipy.oauth2 import SpotifyOAuth
 
-date = input("Please enter the date. YYYY-MM-DD: ")
-URL = f"https://www.billboard.com/charts/hot-100/{date}/"
-header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"}
+time_input = input("Enter the time you want to jump to in this format YYYY-MM-DD: ")
 
-response = requests.get(url=URL, headers=header)
+URL = "https://www.billboard.com/charts/hot-100/"+time_input
 
-soup = BeautifulSoup(response.text, "html.parser")
-container = soup.select(".o-chart-results-list-row-container")
+TEMP1 = "c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only"
+TEMP2 = "c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 u-font-size-23@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-245 u-max-width-230@tablet-only u-letter-spacing-0028@tablet"
+TEMP3 = "c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max u-line-height-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellipsis-2line u-max-width-330 u-max-width-230@tablet-only"
+TEMP4 = "c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max u-line-height-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellipsis-2line u-max-width-330 u-max-width-230@tablet-only u-font-size-20@tablet"
+# These are classes scrapped from the billboard website
 
-songs_list = [item.find(name="h3", id="title-of-a-story").getText().strip() for item in container]
-artists_list = [item.find(name="h3", id="title-of-a-story").parent.span.getText().strip() for item in container]
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+REDIRECT_URI = "http://example.com"
+SPOTIFY_URL = "https://api.spotify.com/v1"
+USERNAME = ""
 
-print(songs_list)
-print(artists_list)
+# ------------- SCRAPE BILLBOARD AND GET 2 LISTS OF SONGS AND CORRESPONDING ARTISTS -------------
+response = requests.get(URL)
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+songs = [item.getText().strip() for item in soup.find_all(name="h3", id="title-of-a-story", class_=TEMP1)]
+first_song = soup.find(name="h3", id="title-of-a-story", class_=TEMP2).getText().strip()
+songs.insert(0, first_song)
+artists = [item.getText().strip() for item in soup.find_all(name="span", class_=TEMP3)]
+first_artist = soup.find(name="span", class_=TEMP4).getText().strip()
+artists.insert(0, first_artist)
 
+# ------------- ACCESS SPOTIFY AND MAKE AUTH -------------
+scope = "playlist-modify-private"
 sp = spotipy.Spotify(
-  auth_manager = SpotifyOAuth(
-    scope="playlist-modify-private",
-    redirect_uri="http://example.com",
-    client_id=<>,
-    client_secret=<>,
-    show_dialogue=True,
-    cache_path="token.txt",
-    username=<displayname>,
-  )
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        cache_path="token.txt",
+        scope=scope,
+        show_dialog=True,
+        username=USERNAME,
+    )
 )
 user_id = sp.current_user()["id"]
+
+# ------------- CREATE PLAYLIST -------------
+playlist = sp.user_playlist_create(
+    user=user_id,
+    name="Top 100 Billboard songs",
+    public=False,
+    collaborative=False,
+    description=f"Top 100 Billboard songs on {time_input}",
+)
+playlist_id = playlist["id"]
+
+# ------------- SEARCH FOR SONGS -------------
+
+
+class NoSongFound(Exception):
+    pass
+
+
+uris = []
+for index in range(0, len(songs)):
+    try:
+        search = sp.search(
+            q=f"track:{songs[index]} artist:{artists[index]}",
+            limit=1,
+            offset=0,
+            market=None,
+            type="track",
+        )
+        if search["tracks"]["total"] == 0:
+            raise NoSongFound
+    except NoSongFound:
+        pass
+    else:
+        uri = str(search["tracks"]["items"][0]["id"])
+        uris.append(uri)
+
+# ------------- ADD SONGS -------------
+sp.playlist_add_items(playlist_id=playlist_id, items=uris)
+
+print(sp.playlist_items(
+    playlist_id=playlist_id,
+    additional_types="track",
+))
